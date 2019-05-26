a038ed3a5eb2aee5784a96452741a479f5f7416c
Aarif Sumra <aarif.sumra@gmail.com>
2019-05-26 16:44:14 +0900

    remove unused code

--- sample-ios-swift/ViewController.swift
+++ sample-ios-swift/ViewController.swift
@@ -28,9 +28,4 @@ class ViewController: UIViewController {
                 }
             })
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 }
